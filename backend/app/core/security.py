51ed9fefaa69e9c878045f93017c94b2063527b3
BAHUMICK <bahumick1@gmail.com>
2026-07-26 21:27:47 +0530

    Implement JWT authentication and secure user login

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -1,4 +1,10 @@
 from passlib.context import CryptContext
+from jose import jwt
+from datetime import datetime, timedelta
+
+SECRET_KEY = "bahumick_lifeos_2026_super_secret_key_12345"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 2
 
 pwd_context = CryptContext(
     schemes=["bcrypt"],
@@ -10,3 +16,16 @@ def hash_password(password: str):
 
 def verify_password(plain_password: str, hashed_password: str):
     return pwd_context.verify(plain_password, hashed_password)
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes= ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+
+    encode_jwt = jwt.encode(
+        to_encode,
+        SECRET_KEY,
+        algorithm = ALGORITHM
+    )
+
+    return encode_jwt
--- backend/app/routers/auth.py
+++ backend/app/routers/auth.py
@@ -6,7 +6,7 @@ from app.schemas.user import UserLogin
 from app.models.user import User
 from app.database.dependencies import get_db
 
-from app.core.security import hash_password, verify_password
+from app.core.security import hash_password, verify_password, create_access_token
 
 router = APIRouter()
 
@@ -46,11 +46,17 @@ def login(user: UserLogin, db: Session = Depends(get_db)):
                     status_code= 400,
                     detail = "invalid email or password"
         )
+    print("Stored password:", existing_user.password)
     if not verify_password(user.password, existing_user.password):
         raise HTTPException(
             status_code=400,
             detail=" invalid email or password"
         )
+    access_token = create_access_token(
+        data={"sub":existing_user.email}
+        )
     return {
-        "message":"login successfull"
-    }
+    "access_token": access_token,
+    "token_type": "bearer"
+}   
+
